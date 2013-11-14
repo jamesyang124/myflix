f4d43ffe7173cad3a4212d1c2e5185ea3a1276b5
James Yang <jamesyang124@gmail.com>
2013-11-14 11:48:36 +0800

    mod2 HW1, concern about escape characters of query string.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,13 @@
 class Video < ActiveRecord::Base 
   belongs_to :category
   validates_presence_of :title, :description
+
+  def self.search_by_title(str)
+    if str.blank? || str.match(/[%|_|!]/)
+      []
+    else
+      # Postgre SQL syntax
+      self.where(["LOWER(title) LIKE LOWER(?)", "%#{str}%"])
+    end
+  end
 end
