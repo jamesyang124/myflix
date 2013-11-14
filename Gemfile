3a867bc27cbd55828e972280e386f717826c0214
James Yang <jamesyang124@gmail.com>
2013-11-14 20:48:22 +0800

    mod2 HW3, done sort, need to limit to 6 videos

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ end
 
 group :test do 
   gem 'shoulda-matchers'
+  gem 'factory_girl_rails'
 end
 
 group :production do
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,11 @@ class Category < ActiveRecord::Base
   has_many :videos, order: :title
 
   validates :name, presence: true, uniqueness: true
+
+  def recent_videos
+    @videos = self.videos
+    @videos.sort! do |x,y| 
+      y.updated_at.to_i <=> x.updated_at.to_i
+    end 
+  end
 end
