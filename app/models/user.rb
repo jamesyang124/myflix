8b7d35c5b29073f3606b592ffd26b8e77951697a
James Yang <jamesyang124@gmail.com>
2013-11-18 00:01:15 +0800

    fix typo for has_secure_password "validattions" to validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base 
 
-  has_secure_password validattions: false
+  has_secure_password validations: false
   
   validates :password, on: :create, length: {minimum: 8}
   validates_presence_of :full_name, :email
