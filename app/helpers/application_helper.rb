721cdba917f4a98a9bc401e893d27cd06df71824
James Yang <jamesyang124@gmail.com>
2013-11-19 18:50:53 +0800

    refactoring search haml code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     options = args.extract_options!
     simple_form_for(object, *(args << options.merge(html: {class: "form-horizontal"})), &block)
   end
+
+  def videos_by_category(category) 
+    items = @videos.select do |v| 
+              v.category == category
+            end.first(6)
+  end
 end
