13a9957960eb22470ceb1f2405f56f1e3743f908
James Yang <jamesyang124@gmail.com>
2013-12-10 21:38:28 +0800

    hidden '+ My Queue' button if video has been queued.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
   def options_for_video_reviews(selected = nil)
     options_for_select((1..5).map{ |number| [pluralize(number,"Star"), number.to_s]}, selected)
   end
+
+  def find_video_by_user_queue_items(video = nil)
+    current_user.queue_items.find_by(video_id: video)
+  end
 end
