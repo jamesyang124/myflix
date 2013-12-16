ab1c6f32352c95aa1297f76d1d6d33f6875362ca
James Yang <jamesyang124@gmail.com>
2013-12-17 00:53:44 +0800

    mod7 hw4 finished, feature spec for user following

--- spec/features/user_following_spec.rb
+++ spec/features/user_following_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 feature 'User Following' do 
-  
+  scenario "user follow and unfollow someone" do 
+    
+    another_user = create(:user)
+    category = Fabricate(:category)
+    video = create(:video, category: category)
+    create(:review, user: another_user, video: video)
+
+    sign_in
+    click_on_video_on_homepage(video)
+
+    click_link(another_user.full_name)
+    click_link "Follow"
+    expect(page).to have_content(another_user.full_name)
+
+    unfollows
+    expect(page).not_to have_content(another_user.full_name)
+  end
+
+  def unfollows
+    find("a[data-method='delete']").click 
+  end
 end
--- spec/support/macro.rb
+++ spec/support/macro.rb
@@ -21,3 +21,7 @@ def sign_in(a_user = nil)
   fill_in 'password', with: user.password
   click_button 'Sign In'
 end
+
+def click_on_video_on_homepage(video)
+  find("a[href='/videos/#{video.id}']").click
+end
