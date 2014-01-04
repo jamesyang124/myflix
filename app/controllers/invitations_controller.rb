fbc433d9a7573b67bffabeb417d94aef8ea24c71
James Yang <jamesyang124@gmail.com>
2014-01-04 15:59:21 +0800

    mod9, invitation post action

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,10 +1,24 @@
 class InvitationsController < ApplicationController 
   before_action :require_user
+  
   def new
     @invitation = Invitation.new
   end
 
   def create
-    
+    invitation = Invitation.new(invitation_params.merge!(inviter_id: current_user.id))
+    if invitation.save 
+      flash[:sucess] = "Your invitation has been sent!"
+      redirect_to home_path
+    else 
+      flash.now[:error] = "Your invitation request has been failed, please check and try again later."
+      render :new
+    end 
+  end
+
+private
+
+  def invitation_params
+    params.require(:invitation).permit!
   end
 end
--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -13,4 +13,35 @@ describe InvitationsController do
       expect(assigns(:invitation)).to be_instance_of(Invitation)
     end
   end
+
+  describe "POST invitations#create" do
+    it_behaves_like "require_sign_in" do 
+      let(:action) { post :create }
+    end
+
+    describe 'with valid input' do
+
+      before :each do 
+        set_current_user
+        post :create, invitation: \
+        { message: Faker::Lorem.paragraph(3),
+          recipient_email: Faker::Internet.email,
+          recipient_name: Faker::Name.name 
+        } 
+      end
+
+      it "redirects to home_path if successed" do 
+        expect(response).to redirect_to home_path
+      end
+
+      it 'creates a new record in "invitations" table' do 
+        expect(Invitation.count).to eq(1)
+      end
+    end
+
+    context 'with invalid input' do 
+
+    end
+  end
+
 end
