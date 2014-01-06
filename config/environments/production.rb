0563444e336ceae9400987b216f402c7068c9940
James Yang <jamesyang124@gmail.com>
2014-01-06 22:36:14 +0800

    MAILER URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,9 +78,10 @@ Myflix::Application.configure do
      :authentication => :plain,
      :address => ENV['MAILGUN_SMTP_SERVER'],
      :port => ENV['MAILGUN_SMTP_PORT'],
-     :domain => "myflix-jy124.herokuapp.com",
+     :domain => ENV["MAILER_URL"],
      :user_name => ENV["MAILGUN_SMTP_LOGIN"],
      :password => ENV["MAILGUN_SMTP_PASSWORD"]
   }
+  config.action_mailer.default_url_options = { :host => ENV["MAILER_URL"] }
 
 end
