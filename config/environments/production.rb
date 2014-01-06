9a2f0b1ebd38afa9e91bdaf6b4bce0aad61e318f
James Yang <jamesyang124@gmail.com>
2014-01-06 22:25:46 +0800

    smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,9 +76,9 @@ Myflix::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
      :authentication => :plain,
-     :address => "smtp.mailgun.org",
-     :port => 587,
-     :domain => ENV["MAILGUN_DOMAIN"],
+     :address => ENV['MAILGUN_SMTP_SERVER'],
+     :port => ENV['MAILGUN_SMTP_PORT'],
+     :domain => "myflix-jy124.herokuapp.com",
      :user_name => ENV["MAILGUN_SMTP_LOGIN"],
      :password => ENV["MAILGUN_SMTP_PASSWORD"]
   }
