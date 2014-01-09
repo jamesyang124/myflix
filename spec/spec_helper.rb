8cdcb844a1c5cc9d5e4b86e7aace750c5f728eed
James Yang <jamesyang124@gmail.com>
2014-01-09 22:46:45 +0800

    Fix rspec bugs. Now Sidekiq execute jobs immediately when jobs enqueue for rspec testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,12 @@ require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/email/rspec'
 
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
+
+# execute job immeditaley when in queue.
+Sidekiq::Testing.inline!
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
