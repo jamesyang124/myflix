b61071411eddc987c460118be3a46e010bba61f2
James Yang <jamesyang124@gmail.com>
2014-01-19 21:59:31 +0800

    mod11 hw2 (1) done, handle and test :create request without file uploading.

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,6 +6,19 @@ class Admin::VideosController < ApplicationController
   end
 
   def create
-    
+    @video = Video.new(create_video)
+    if @video.save
+      flash[:success] = "You have successfully added the new video #{@video.title}."
+      redirect_to new_admin_video_path
+    else
+      flash[:error] = "You are failed to add the video, please try again."
+      render :new
+    end
+  end
+
+  private
+
+  def create_video
+    params.require(:video).permit!
   end
 end
