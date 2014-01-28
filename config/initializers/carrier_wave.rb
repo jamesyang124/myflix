432d4b66b6311445e9bb6ad8189026985237713c
James Yang <jamesyang124@gmail.com>
2014-01-28 22:54:01 +0800

    rename fog_host to asset_host for carrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
  
     config.fog_use_ssl_for_aws = false
     config.fog_directory    = ENV['S3_BUCKET_NAME']
-    config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+    config.asset_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
   else 
     config.storage = :file 
     config.enable_processing = Rails.env.development?
