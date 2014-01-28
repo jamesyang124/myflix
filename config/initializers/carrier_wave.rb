4fe86dc585c254392b897fcb273a3d59aca6bf6d
James Yang <jamesyang124@gmail.com>
2014-01-29 00:12:41 +0800

    heroku for carrierwave config.ru

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,11 +7,10 @@ CarrierWave.configure do |config|
       :aws_secret_access_key  => ENV["S3_SECRET"],         
     }
     config.cache_dir = "#{Rails.root}/tmp/uploads"                  
-    # To let CarrierWave work on heroku
- 
+
     config.fog_use_ssl_for_aws = false
     config.fog_directory    = ENV['S3_BUCKET_NAME']
-    config.asset_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
+    config.asset_host       = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
   else 
     config.storage = :file 
     config.enable_processing = Rails.env.development?
