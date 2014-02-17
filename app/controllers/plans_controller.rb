fdcc0e418d9afbf9ba6ff574e057929231292f22
James Yang <jamesyang124@gmail.com>
2014-02-17 10:31:47 -0800

    modified plans_controller rspec test, mock StripWrapper::retrieve behavior, and add stub_chain to it.

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,16 +3,29 @@ class PlansController < ApplicationController
 
   def index
     @payments = Payment.where(user_id: current_user)
-    if @subscriptions = StripeWrapper::Customer.retrieve(current_user.customer_token)
-       @recent_sub_plan = @subscriptions.data.last.plan.name
-       @recent_sub_period_end = Time.at(@subscriptions.data.last.current_period_start).strftime("%m/%d/%Y")
-       @recent_sub_amount = @subscriptions.data.last.plan.amount/100.0
-    else
-       redirect_to root_path
+    customer_token = current_user.customer_token
+    unless customer_token and retrieve_stripe_subscriptions(customer_token)
+      flash[:info] = "We can not find your billing information, please contact customer service."
+      redirect_to root_path
     end
   end
 
   def create
     
+  end
+
+  private
+
+  def retrieve_stripe_subscriptions(customer_token = nil) 
+    begin
+      @subscriptions = StripeWrapper::Customer.retrieve(customer_token)
+      @recent_sub_plan = @subscriptions.data.last.plan.name
+      @recent_sub_period_end = Time.at(@subscriptions.data.last.current_period_end).strftime("%m/%d/%Y")
+      @recent_sub_amount = @subscriptions.data.last.plan.amount/100.0
+      true
+    rescue Stripe::CardError => e 
+      flash[:info] = "We can not find your billing information, please contact customer service."
+      false
+    end
   end
 end
--- spec/controllers/plans_controller_spec.rb
+++ spec/controllers/plans_controller_spec.rb
@@ -2,27 +2,32 @@ require 'spec_helper'
 
 describe PlansController do 
   describe "GET #index" do 
-        
     it "set @payments for current user", :vcr do
-      user = Fabricate(:user, customer_token: "some_token")
+      user = Fabricate(:user, customer_token: "somt_token")
+      Payment.create(amount: 999, user: user)
       set_current_user(user)
-      
-      subscriptions = double(:list_subscriptions_data)
-      StripeWrapper::Customer.should_receive(:retrieve).and_return(subscriptions)
+    
+      PlansController.any_instance.should_receive(:retrieve_stripe_subscriptions).with(user.customer_token).and_return(true)
 
       get :index
-      expect(assigns(:payments)).to match_array(Payment.where(id: user))
+      expect(assigns(:payments)).to match_array(Payment.where(user_id: user))
     end
 
     it "retrieve a list of subscription history from Stripe", :vcr do
       user = Fabricate(:user, customer_token: "some_token")
       set_current_user(user)
 
-      subscriptions = double(:list_subscriptions_data)
-      StripeWrapper::Customer.should_receive(:retrieve).and_return(subscriptions)
+      subscriptions = double(:valid_list)
+      subscriptions.stub_chain(:data, :last, :plan, :name).and_return("myflix_base_plan")
+      subscriptions.stub_chain(:data, :last, :current_period_end).and_return(1392446068)
+      subscriptions.stub_chain(:data, :last, :plan, :amount).and_return(999)
+
+      StripeWrapper::Customer.should_receive(:retrieve).with(user.customer_token).and_return(subscriptions)
 
       get :index
-      expect(assigns(:subscriptions)).to be_present       
+      expect(assigns(:recent_sub_plan)).to eq("myflix_base_plan")  
+      expect(assigns(:recent_sub_period_end)).to eq(Time.at(1392446068).strftime("%m/%d/%Y"))
+      expect(assigns(:recent_sub_amount)).to eq(9.99)       
     end
 
     it "render plans/index template", :vcr do
@@ -30,11 +35,21 @@ describe PlansController do
       set_current_user(user)
       Payment.create(amount: 999, user: user)
 
-      subscriptions = double(:list_subscriptions_data)
-      StripeWrapper::Customer.should_receive(:retrieve).and_return(subscriptions)
+      PlansController.any_instance.should_receive(:retrieve_stripe_subscriptions).with(user.customer_token).and_return(true)
 
       get :index
       expect(response).to render_template :index
+    end
+
+    it "redirect to root_path if customer_token is nil or cannot find data from stripe", :vcr do
+      user = Fabricate(:user, customer_token: "wrong")
+      set_current_user(user)
+      Payment.create(amount: 999, user: user)
+
+      PlansController.any_instance.should_receive(:retrieve_stripe_subscriptions).with(user.customer_token).and_return(false)
+
+      get :index
+      expect(response).to redirect_to root_path
     end
   end
 
