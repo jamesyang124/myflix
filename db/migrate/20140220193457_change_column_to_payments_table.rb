b893c4449f5ca73c53b32ad13b00f87feff8c0b7
James Yang <jamesyang124@gmail.com>
2014-02-27 14:31:53 -0800

    fix migration file

--- db/migrate/20140220193457_change_column_to_payments_table.rb
+++ db/migrate/20140220193457_change_column_to_payments_table.rb
@@ -2,7 +2,7 @@ class ChangeColumnToPaymentsTable < ActiveRecord::Migration
   def change
     remove_column :payments, :create_at, :string
     remove_column :payments, :start_date, :string
-    remove_column :payments, :end_at, :string
+    remove_column :payments, :end_date, :string
 
     add_column :payments, :create_at, :integer
     add_column :payments, :start_date, :integer
